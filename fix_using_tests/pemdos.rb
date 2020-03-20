08bb513fcb16eae574773f9f6d79a9ecfac4ec4a
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-20 22:31:05 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def snake_it_up(string)
-  if string[0] == "s"
-  10 * "s" + string
-  else
-  string
-  end
+   if string[0] == "s"
+     "ssssssssss" + string
+    else
+      string
+    end
 end
+
--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,17 +1,13 @@
-
+require 'pry'
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
-    end
-  end
+    turtles.map { |turtle| turtle[:traits] }
 end
