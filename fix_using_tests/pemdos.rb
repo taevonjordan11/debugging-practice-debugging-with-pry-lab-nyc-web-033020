6236d0a4d15247b161d4d243f6b305e848c7448f
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-20 22:31:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,20 @@
 require 'pry'
 
 def snake_it_up(string)
+<<<<<<< HEAD
    if string[0] == "s"
      "ssssssssss" + string
     else
       string
     end
+=======
+  if string[0] == "s"
+  # 10 * "s" + string
+  count = 0
+  while count < 10 do
+    string.prepend("s")
+      count += 1
+  end
+>>>>>>> dc69cc999788d386162cb255202ac47fbb297af9
 end
 
